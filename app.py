917a8551d60274deb95d631a70523877407066bf
Ragav Kumar V <ragavkumarv@gmail.com>
2026-03-03 17:17:23 +0530

    wrong password

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 # save this as app.py
 from flask import Flask
 from routes.movies_bp import movies_bp
+from config import Config
+from extensions import db
+from sqlalchemy.sql import text
 
 
 app = Flask(__name__)
+app.config.from_object(Config)  # URL
 
+db.init_app(app)
+
+
+with app.app_context():
+    try:
+        result = db.session.execute(text("SELECT 1")).fetchall()
+        print("Connection successful:", result)
+    except Exception as e:
+        print("Error connecting to the database:", e)
 
 # Task
 # /api/movies - JSON
--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = ""
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://postgres:yourpassword@localhost:5432/movie_bcs_db_dec_2025"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
