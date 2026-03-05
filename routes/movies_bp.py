b11b96ea6f3021c06cf744b1903918ee69f57651
Ragav Kumar V <ragavkumarv@gmail.com>
2026-03-05 12:29:04 +0530

    post

--- routes/movies_bp.py
+++ routes/movies_bp.py
@@ -1,5 +1,5 @@
 # from package_name import function
-from flask import Blueprint
+from flask import Blueprint, request
 from models.movie import Movie
 from extensions import db
 from sqlalchemy import select
@@ -30,7 +30,7 @@ def get_all_movies():
 
 # id -> variable
 # {"message": "movie not found"}
-@movies_bp.get("/<id>")
+@movies_bp.get("/<id>")  # API Endpoint
 def get_movie_by_id(id):
     # Select * from movies where id = 100
     data = db.session.get(Movie, id)
@@ -39,6 +39,9 @@ def get_movie_by_id(id):
         return {"message": "movie not found"}, HTTP_NOT_FOUND
 
     return data.to_dict()
+
+
+# get_movie_by_id -> Controller
 
 
 @movies_bp.delete("/<id>")
@@ -56,3 +59,29 @@ def delete_movie_by_id(id):
         return {"message": str(err)}, HTTP_SERVER_ERROR
 
     return {"data": movie.to_dict(), "message": "movie delete successfully"}
+
+
+@movies_bp.post("/")
+def create_movie():
+    # Data -> body as json
+    data = request.get_json()
+
+    new_movie = Movie(
+        name=data["name"],
+        poster=data["poster"],
+        summary=data["summary"],
+        rating=data["rating"],
+        trailer=data["trailer"],
+    )
+
+    try:
+        db.session.add(new_movie)  # temp
+        db.session.commit()  # permanent
+    except Exception as err:
+        db.session.rollback()  # Undo
+        return {"message": str(err)}, HTTP_SERVER_ERROR
+
+    return {"data": new_movie.to_dict(), "message": "movie added successfully"}
+
+    # print(data)
+    # return data
